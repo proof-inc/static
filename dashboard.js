e3783f1f9703f96dd640152656d2ce717849789b
Luuk de Waal Malefijt <luuk@waalmalefijt.nl>
2018-07-09 01:08:48 +0200

    initial version of webflow client side

---
+++ dashboard.js
@@ -0,0 +1,285 @@
+//
+// CONFIG & INIT
+//
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: DASHBOARD_API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DB_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSENGER_SENDING_ID
+});
+
+// TODO: capitalise?
+var sliderMinInvestment = 1000;
+var sliderSalePhase = 0;
+
+const strokeWidth = 25;
+const emptyColor = '#cc0000';
+const fullColor = '#26CA7B';
+const backgroundColor = '#DDD';
+const barTextFont = '"Raleway", Helvetica, sans-serif';
+
+const BASE_TOKEN_AMOUNT = 120 * 1000000;
+var TOTAL_TOKENS_SOLD = 0;
+
+// TODO: vervangen met echt unieke Auth0 id
+String.prototype.hashCode = function(){
+  var hash = 0;
+  if (this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i);
+    hash = ((hash<<5)-hash)+char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+// on document ready
+$(function() {
+
+  // do the following only if we are logged in using Auth0
+	handleAuthentication(login, function() {
+
+  	var email = localStorage.getItem('email');
+    var name = localStorage.getItem('name');
+  	var emailHash = email.hashCode();
+
+  	var db = firebase.database();
+    var investors = db.ref('investors');
+  	var thisInvestor = db.ref('investors').child(emailHash);
+
+    var kycNoticeSrc = $('#kyc-notice a')
+    	.attr('href')
+      .replace("{{INVESTOR_EMAIL}}", email)
+      .replace("{{IDENTIFICATION_CODE}}", emailHash);
+
+  	$('#welcome-name').html(name);
+    $('#kyc-notice a').attr('href', kycNoticeSrc);
+
+  	// init investor entry
+  	thisInvestor.once('value', function(snapshot) {
+      var exists = (snapshot.val() !== null);
+      if (!exists) {
+      	console.log("initialized entry for investor: " + email);
+      	thisInvestor.set({
+        	kycDone: false,
+          euroInvested: 0
+        });
+      }
+    });
+
+  	// listen to changes on the euro invested field
+  	db.ref('investors/' + emailHash + '/euroInvested').on('value', function(snapshot) {
+      //alert("investor amount changed!");
+      updateInvestorEuroInvested(snapshot.val());
+    });
+
+    // listen to changes on the euro invested field
+  	db.ref('stats/euroInvested').on('value', function(snapshot) {
+    	//alert("euroinvested changed!");
+    	TOTAL_TOKENS_SOLD = euroToTokenAmount(snapshot.val());
+      updateTotalEuroInvested(snapshot.val());
+    }, function(error) {console.error(error);});
+
+  	//
+    var tokenShareBar = new ProgressBar.SemiCircle('#progress-share-percentage', {
+      strokeWidth: strokeWidth,
+      color: '#FFEA82',
+      trailColor: backgroundColor,
+      trailWidth: 25,
+      easing: 'easeInOut',
+      duration: 1400,
+      svgStyle: null,
+      text: {
+        value: '',
+        alignToBottom: false
+      },
+      from: {color: emptyColor},
+      to: {color: fullColor},
+      // Set default step function for all animate calls
+      step: (state, bar) => {
+        bar.path.setAttribute('stroke', state.color);
+        var value = Math.round(bar.value() * 100);
+        if (value > 1) {
+          bar.setText(value);
+        }
+        else {
+          bar.setText(0);
+        }
+
+        bar.text.style.color = state.color;
+      }
+    });
+    tokenShareBar.text.style.fontFamily = barTextFont;
+    tokenShareBar.text.style.fontSize = '2rem';
+
+    // token supply stats
+    var tokenSupplyBar = new ProgressBar.Line('#token-supply-left-progress', {
+      strokeWidth: strokeWidth,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#F8BC3F',
+      trailColor: '#eee',
+      trailWidth: 1,
+      svgStyle: {width: '100%', height: '100%'},
+      text: {
+        style: {
+          color: '#333',
+          //position: 'absolute',
+          //right: '0',
+          //top: '50px',
+          "text-align": "center",
+          padding: 0,
+          margin: '-25px',
+          "font-size": "50px",
+          "font-weight": "bold",
+          transform: null
+        },
+        autoStyleContainer: false
+      },
+      from: {color: emptyColor},
+      to: {color: fullColor},
+      step: (state, bar) => {
+        bar.setText(Math.round(bar.value() * 100) + ' %');
+        bar.path.setAttribute('stroke', state.color);
+      }
+    });
+
+    tokenSupplyBar.text.style.fontFamily = barTextFont;
+
+    // sliders
+    // TODO: max aanpassen wanneer nieuwe investeringen gedaan worden
+    $("#investment-slider").ionRangeSlider({
+      min: 1000,
+      step: 5000,
+      grid: true,
+      max: tokensSaleAvailable()/4,
+      from: sliderMinInvestment,
+      prefix: '€',
+
+      onChange: function (data) {
+        sliderMinInvestment = data.from;
+        updateTokensToReceive();
+      },
+    });
+
+    $("#sale-phase-slider").ionRangeSlider({
+      min: 0,
+      max: 100,
+      from: sliderSalePhase,
+
+      onChange: function (data) {
+        sliderSalePhase = data.from;
+        updateTokensToReceive();
+      },
+    });
+
+		$("#investment-manual-amount").change(function(){
+    	sliderMinInvestment = $(this).val();
+      $("#investment-slider")
+      	.data("ionRangeSlider")
+        .update({from: sliderMinInvestment});
+      updateTokensToReceive();
+    });
+
+		$("#log-out").click(logout);
+
+    function updateTokensToReceive() {
+      var baseTokenCount = euroToTokenAmount(sliderMinInvestment);
+      var totalTokensSold = (sliderSalePhase / 100) * BASE_TOKEN_AMOUNT;
+      var tokenCount = baseTokenCount + tokenBonusAmount(baseTokenCount, totalTokensSold);
+      $(".investment-token-result").text("" + tokenCount);
+      $("#investment-manual-amount").val(sliderMinInvestment);
+    };
+
+		function tokensSaleAvailable() {
+    	return BASE_TOKEN_AMOUNT - TOTAL_TOKENS_SOLD;
+    }
+
+		function percentSold(tokenAmount) {
+    	return 1/((BASE_TOKEN_AMOUNT / tokenAmount))*100;
+    }
+
+		function tokenBonusAmount(tokenAmount, totalTokensSold) {
+    	var tokenAmountModifier = getBonusModifier(totalTokensSold);
+      var bonusModifier = ((tokenAmountModifier * 1000) - 1000) / 1000; // due to strange rounding error
+    	return Math.round(tokenAmount * bonusModifier);
+    }
+
+		function getBonusModifier(totalTokensSold) {
+    	var bonusModifier = 1.0;
+      var salePhase = percentSold(totalTokensSold);
+      if (salePhase < 2) {
+
+      }
+      else if (salePhase < 10) {
+        bonusModifier = 1.1;
+      }
+      else if (salePhase < 25) {
+        bonusModifier = 1.2;
+      }
+      else if (salePhase < 50) {
+        bonusModifier = 1.3;
+      }
+      else if (salePhase < 95) {
+        bonusModifier = 1.4;
+      }
+      else {
+        bonusModifier = 1.5;
+      }
+      return bonusModifier;
+    }
+
+		function updateInvestorEuroInvested(amount) {
+      var tokenAmount = euroToTokenAmount(amount);
+    	var shareModifier = percentSold(tokenAmount) / 100;
+      var tokenBonus = tokenBonusAmount(tokenAmount);
+      var totalTokenAmount = tokenAmount + tokenBonus;
+      tokenShareBar.animate(shareModifier);
+      $('#balance-total').text(tokenAmount);
+      $('#bonus-total').text(tokenBonus);
+    }
+
+    function updateTotalEuroInvested(amount) {
+    	var tokenAmount = euroToTokenAmount(amount);
+    	var modifier = percentSold(tokenAmount) / 100;
+      tokenSupplyBar.animate(1 - modifier);
+    }
+
+		function euroToTokenAmount(euroAmount) {
+    	return euroAmount * 4;
+    }
+
+		// init states
+		//tokenShareBar.animate(0.01);
+    //tokenSupplyBar.animate(1.0);
+    updateTokensToReceive();
+  });
+
+});
+
+var _support = _support || { 'ui': {}, 'user': {} };
+_support['account'] = 'troovebird';
+_support['ui']['contactMode'] = 'mixed';
+_support['ui']['enableKb'] = 'true';
+_support['ui']['styles'] = {
+  widgetColor: 'rgb(24, 162, 221)',
+};
+_support['ui']['widget'] = {
+  label: {
+    text: 'Let us know if you have any questions! 😊',
+    mode: "notification",
+    delay: 3,
+    duration: 30,
+    sound: true,
+  },
+  position: 'bottom-right',
+};
+_support['apps'] = {
+  faq: {"enabled":true},
+  recentConversations: {},
+  orders: {}
+};
---
+++ site-wide.js
@@ -0,0 +1,73 @@
+var webAuth = new auth0.WebAuth({
+  domain: AUTH0_DOMAIN,
+  clientID: AUTH0_CLIENT_ID,
+  redirectUri: AUTH0_CALLBACK_URL,
+  audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+  responseType: 'token id_token',
+  scope: 'openid email profile'
+});
+
+function setSession(authResult) {
+  // Set the time that the Access Token will expire at
+  var expiresAt = JSON.stringify(
+    authResult.expiresIn * 1000 + new Date().getTime()
+  );
+  localStorage.setItem('access_token', authResult.accessToken);
+  localStorage.setItem('id_token', authResult.idToken);
+  localStorage.setItem('expires_at', expiresAt);
+  localStorage.setItem('email', authResult.idTokenPayload.email);
+  localStorage.setItem('name', authResult.idTokenPayload.name);
+  localStorage.setItem('locale', authResult.idTokenPayload.locale);
+  localStorage.setItem('picture', authResult.idTokenPayload.picture);
+
+  console.log(authResult.idTokenPayload);
+}
+
+function login() {
+	webAuth.authorize();
+}
+
+function logout(onLogout) {
+  // Remove tokens and expiry time from localStorage
+  localStorage.removeItem('access_token');
+  localStorage.removeItem('id_token');
+  localStorage.removeItem('expires_at');
+  if (onLogout && typeof onLogout === "function") {
+    onLogout();
+  }
+}
+
+function isAuthenticated() {
+  // Check whether the current time is past the
+  // Access Token's expiry time
+  var expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+  return new Date().getTime() < expiresAt;
+}
+
+function toDashboard() {
+	location.href = '/dashboard/overview';
+}
+
+function toHome() {
+	location.href = '/';
+}
+
+function handleAuthentication(onFail, onSuccess) {
+  webAuth.parseHash(function(err, authResult) {
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      window.location.hash = '';
+      setSession(authResult);
+    }
+    else if (err) {
+      console.error("Error trying to log in! (" + err + ")");
+    }
+
+    if (isAuthenticated()) {
+      if (onSuccess && typeof onSuccess === "function")
+    	  onSuccess();
+    }
+    else if (onFail && typeof onFail === "function") {
+    	onFail();
+    }
+  });
+}
